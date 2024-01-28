fe0077156f36deee087df7ee1d09d07085fd849a
Gokul230504 <gokulrajl.21ece@kongu.edu>
2024-01-28 12:03:13 +0530

    Updated

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import './App.css';
 import HomeComp from './components/home';
 import { Routes,Route } from 'react-router-dom';
 import RefeRence from './components/userref';
+import Memo from './components/usememo';
 function App() {
   return (
     <div className="App">
       <header className="App-header">
        <HomeComp/>
        <RefeRence/>
+       <Memo/>
       </header>
       
     </div>
--- src/components/usememo.jsx
+++ src/components/usememo.jsx
@@ -1,6 +1,7 @@
 import React,{useMemo, useState} from "react";
 const Memo=()=>{
-    const [number,UpdateNumber]=useState(0)
+    const [number,UpdateNumber]=useState(0);
+    const [dark, updatetheme]=useState(false);
     const doubleNumber = doubleNumberSlow(number);
     const doubleNumberSlow=(num)=>{
         for(let i=0;i<1000000000000000;i++)
@@ -8,10 +9,17 @@ const Memo=()=>{
          return num*2;
         }
     }
+    const Theme = useMemo(()=>{
+        return {
+        backgroundColor:dark?"black":"white",
+        color:dark?"white":"black"
+    }
+    },[dark])
     return(
         <><h1>Number type{number} </h1>
-        <input type="number" value></input>
-        <h2>The doubled number is{doubleNumber}</h2>
+        <input type="number" value={number} onClick></input>
+        <button onClick={()=>{updatetheme(dark =>!dark)}}>Change Theme</button>
+        <h2 style={Theme}>The doubled number is{doubleNumber}</h2>
         </>
     )
 }
